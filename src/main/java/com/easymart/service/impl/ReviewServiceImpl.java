89aee61da918520894594d3a8c2aa56264abc474
Sudhanshu Bisht <bishtsudhanshu7@Sudhanshus-MacBook-Air.local>
2026-08-12 12:03:07 +0530

    Email bug resolved

--- src/main/java/com/easymart/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/easymart/service/impl/ReviewServiceImpl.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 @Transactional
 @Service
